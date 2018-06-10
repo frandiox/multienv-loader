90458def565766d558d4906a3d975e0988c14ebb
Fran Dios <frankdiox@gmail.com>
2018-06-11 02:02:16 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,103 @@
+/* MULTIENV: dotenv loader for multiple environments */
+/* Loads env variables from dotenv files: .env, .env.[NODE_ENV], .env.local, .env.[NODE_ENV].local */
+
+const fs = require('fs')
+const path = require('path')
+
+module.exports = {
+  /**
+   * @description Main entry point. Loads env variables from dotenv files
+   * @param {Object=} context
+   * @param {String=} options.mode Mode provided in `process.env.NODE_END`
+   * @param {String=} options.envPath Path of the dotenv files. Defaults to `process.cwd()`
+   * @param {Array=} options.envFiles Array of env files to load. Defaults to `['.env', '.env.[mode]', '.env.local', '.env.[mode].local']`
+   * @param {Boolean=} options.dry Whether it should modify `process.env` or just return the result
+   * @param {Boolean=} options.override Whether it should override variables already defined in `process.env`
+   * @return {Object} Object containing all the key:value from the src files
+   */
+  load({
+    mode = process.env.NODE_ENV || '',
+    envPath = process.cwd(),
+    envFiles = ['.env', `.env.${mode}`, '.env.local', `.env.${mode}.local`],
+    dry = false,
+    override = false,
+  } = {}) {
+    const env = Object.assign.apply(
+      null,
+      [{}].concat(
+        envFiles.map(envFile => this.safeLoad(path.resolve(envPath, envFile)))
+      )
+    )
+
+    if (!dry) this.loadEnv(env, { override })
+
+    return env
+  },
+
+  /**
+   * @description Loads the given dotenv safely, silently ignoring non existing files
+   * @param {String} path Dotenv file path to load
+   * @return {Object} Object containing all the key:value from the src
+   */
+  safeLoad(path = '.env') {
+    try {
+      return this.parse(fs.readFileSync(path, 'utf-8'))
+    } catch (err) {
+      // only ignore error if file is not found
+      if (err.toString().indexOf('ENOENT') < 0) {
+        throw err
+      }
+
+      return {}
+    }
+  },
+
+  /**
+   * @description Loads the given env variables
+   * @param {Object} env Object containing all the key:value from the src
+   * @param {Object=} options
+   * @param {Boolean=} options.override Whether it should override variables already defined in `process.env`
+   */
+  loadEnv(env, { override } = {}) {
+    Object.keys(env).forEach(key => {
+      if (override || process.env[key] === undefined) {
+        process.env[key] = env[key]
+      }
+    })
+  },
+
+  /**
+   * @description Parses a dotenv-like string into an object
+   * @param {String} src Content of a dotenv file (`key=value` lines)
+   * @return {Object} Object containing all the key:value from the src
+   */
+  parse(src = '') {
+    const res = {}
+    src.split('\n').forEach(line => {
+      // matching "KEY' and 'VAL' in 'KEY=VAL'
+      const keyValueArr = line.match(/^\s*([\w.-]+)\s*=\s*(.*)?\s*$/)
+      // matched?
+      if (keyValueArr != null) {
+        const key = keyValueArr[1]
+        let value = keyValueArr[2] || ''
+
+        // expand newlines in quoted values
+        const len = value ? value.length : 0
+        if (
+          len > 0 &&
+          value.charAt(0) === '"' &&
+          value.charAt(len - 1) === '"'
+        ) {
+          value = value.replace(/\\n/gm, '\n')
+        }
+
+        // remove any surrounding quotes and extra spaces
+        value = value.replace(/(^['"]|['"]$)/g, '').trim()
+
+        res[key] = value
+      }
+    })
+
+    return res
+  },
+}
---
+++ test/index.test.js
@@ -0,0 +1,145 @@
+const fs = require('fs')
+const path = require('path')
+const multienv = require('../index')
+
+const dotenvPath = path.resolve(__dirname, 'fixtures', '.env')
+const dotenvContent = fs.readFileSync(dotenvPath, 'utf8')
+const dry = true
+const vars = { TEST1: 'TEST1', TEST2: 'TEST2' }
+const cleanEnv = o => Object.keys(o).forEach(key => delete process.env[key])
+Object.freeze(vars)
+
+describe('@utils/multienv', () => {
+  describe('parse', () => {
+    it('correctly turns string into object', () => {
+      const result = multienv.parse('A=1\nB=2\nC=3')
+      expect(result).toHaveProperty('A', '1')
+      expect(result).toHaveProperty('B', '2')
+      expect(result).toHaveProperty('C', '3')
+    })
+
+    it('parses multiline values', () => {
+      const result = multienv.parse('A=1\nB="1\\n2\\n3"\nC=3')
+      expect(result).toHaveProperty('A', '1')
+      expect(result).toHaveProperty('B', '1\n2\n3')
+      expect(result).toHaveProperty('C', '3')
+    })
+  })
+
+  describe('safeLoad', () => {
+    let spyParse
+
+    beforeAll(() => {
+      spyParse = jest.spyOn(multienv, 'parse')
+      spyParse.mockImplementation(() => vars)
+    })
+
+    afterAll(() => spyParse.mockRestore())
+    beforeEach(() => spyParse.mockClear())
+
+    it('reads files safely', () => {
+      expect(() => multienv.safeLoad(dotenvPath)).not.toThrow()
+      expect(spyParse).toHaveBeenCalledTimes(1)
+
+      expect(() =>
+        multienv.safeLoad(path.resolve(__dirname, 'fixtures', 'non-existing'))
+      ).not.toThrow()
+      expect(spyParse).toHaveBeenCalledTimes(1)
+    })
+
+    it('reads files and calls parser', () => {
+      const res = multienv.safeLoad(dotenvPath)
+      expect(res).toEqual(vars)
+      expect(spyParse).toHaveBeenCalledTimes(1)
+      expect(spyParse).toHaveBeenCalledWith(dotenvContent)
+    })
+
+    it('throws if the error is different from ENOENT', () => {
+      const spyFs = jest.spyOn(fs, 'readFileSync')
+      const enoentError = new Error('ENOENT')
+      const randomError = new Error('Some random error')
+
+      expect(() => multienv.safeLoad('non existing file')).not.toThrow()
+      spyFs.mockImplementationOnce(() => {
+        throw enoentError
+      })
+      expect(() => multienv.safeLoad()).not.toThrow()
+
+      spyFs.mockImplementationOnce(() => {
+        throw randomError
+      })
+      expect(() => multienv.safeLoad()).toThrow(randomError)
+
+      expect(spyParse).not.toHaveBeenCalled()
+
+      spyFs.mockRestore()
+    })
+  })
+
+  describe('loadEnv', () => {
+    const TEST3 = 'TEST3'
+    const MODIFIED = 'modified'
+    const ORIGINAL = 'original'
+    const input = { ...vars, [TEST3]: MODIFIED }
+    const res = expect.objectContaining(vars)
+
+    beforeEach(() => {
+      expect(process.env).not.toEqual(res)
+      process.env.TEST3 = ORIGINAL
+    })
+
+    afterEach(() => cleanEnv(input))
+
+    it('merges all the variables correctly in process.env without overriding', () => {
+      multienv.loadEnv(input)
+
+      expect(process.env).toEqual(res)
+      expect(process.env).toHaveProperty(TEST3, ORIGINAL)
+    })
+
+    it('merges all the variables correctly in process.env overriding', () => {
+      multienv.loadEnv(input, { override: true })
+
+      expect(process.env).toEqual(res)
+      expect(process.env).toHaveProperty(TEST3, MODIFIED)
+    })
+  })
+
+  describe('load', () => {
+    const envPath = path.resolve(__dirname, './fixtures')
+    const mode = 'test'
+    const res = expect.objectContaining({
+      FIRST: '111',
+      SECOND: 'bbb',
+      THIRD: 'ccc',
+      FOURTH: '444',
+      FIFTH: 'eee',
+      SIXTH: '666',
+    })
+
+    it('loads base, mode and local dotenvs', () => {
+      const spySafeLoad = jest.spyOn(multienv, 'safeLoad')
+      spySafeLoad.mockImplementation(() => vars)
+
+      expect(multienv.load({ envPath, mode, dry })).toEqual(vars)
+
+      expect(spySafeLoad).toHaveBeenCalledTimes(4)
+
+      spySafeLoad.mockRestore()
+    })
+
+    it('merges all the dotenv files correctly in the returned value in DRY mode', () => {
+      expect(process.env).not.toEqual(res)
+      expect(multienv.load({ envPath, mode, dry })).toEqual(res)
+      expect(process.env).not.toEqual(res)
+    })
+
+    it('merges all the dotenv files correctly in process.env', () => {
+      expect(process.env).not.toEqual(res)
+      const env = multienv.load({ envPath, mode })
+      expect(env).toEqual(res)
+      expect(process.env).toEqual(res)
+      cleanEnv(env)
+    })
+  })
+})
